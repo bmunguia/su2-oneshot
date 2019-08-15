fb45ecd566331c8a0548725628096323b6b58727
bmunguia <bmunguia10@gmail.com>
2019-08-15 15:36:27 +0200

    Initalize medi in CDriver in case python wrapper is used

--- SU2_CFD/src/drivers/CDriver.cpp
+++ SU2_CFD/src/drivers/CDriver.cpp
@@ -37,6 +37,7 @@
 
 #include "../../include/drivers/CDriver.hpp"
 #include "../../include/definition_structure.hpp"
+#include <cassert>
 
 #ifdef VTUNEPROF
 #include <ittnotify.h>
@@ -46,6 +47,13 @@ CDriver::CDriver(char* confFile,
                  unsigned short val_nZone,
                  SU2_Comm MPICommunicator):config_file_name(confFile), StartTime(0.0), StopTime(0.0), UsedTime(0.0), ExtIter(0), nZone(val_nZone), StopCalc(false), fsi(false), fem_solver(false) {
 
+
+  /*--- Initialize Medipack (must also be here so it is initialized from python) ---*/
+#ifdef HAVE_MPI
+  #if defined(CODI_REVERSE_TYPE) || defined(CODI_FORWARD_TYPE)
+    SU2_MPI::Init_AMPI();
+  #endif
+#endif
 
   unsigned short jZone;
 
